49ac7eb9a5a016f4a86f0a153cad9e55db925b8e
laszlo-ratesic <keenanrc@hotmail.com>
2022-02-16 10:21:53 -0600

    expand HTML generation with map, filter, destructuring, and arrow functions

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,6 @@
-const generateAbout = aboutText => {
+const generateAbout = (aboutText) => {
   if (!aboutText) {
-    return '';
+    return "";
   }
 
   return `
@@ -11,9 +11,9 @@ const generateAbout = aboutText => {
   `;
 };
 
-const generateProjects = projectsArr => {
+const generateProjects = (projectsArr) => {
   // get array of just featured projects
-  const featuredProjects = projectsArr.filter(project => {
+  const featuredProjects = projectsArr.filter((project) => {
     if (project.feature) {
       return true;
     } else {
@@ -22,7 +22,7 @@ const generateProjects = projectsArr => {
   });
 
   // get array of all non-featured projects
-  const nonFeatuedProjects = projectsArr.filter(project => {
+  const nonFeaturedProjects = projectsArr.filter((project) => {
     if (!project.feature) {
       return true;
     } else {
@@ -30,32 +30,18 @@ const generateProjects = projectsArr => {
     }
   });
 
-  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-    return `
-    <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-      <h3 class="portfolio-item-title text-light">${name}</h3>
-      <h5 class="portfolio-languages">
-        Built With:
-        ${languages.join(', ')}
-      </h5>
-      <p>${description}</p>
-      <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-    </div>
+  const featuredProjectHtmlArr = featuredProjects.map(
+    ({ name, description, languages, link }) => {
+      return `
+
     `;
-  });
+    }
+  );
 
   const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
     ({ name, description, languages, link }) => {
       return `
-      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-          Built With:
-          ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-      </div>
+
       `;
     }
   );
@@ -64,8 +50,39 @@ const generateProjects = projectsArr => {
   <section class="my-3" id="portfolio">
     <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
     <div class="flex-row justify-space-between">
-    ${featuredProjectHtmlArr.join('')}
-    ${nonFeaturedProjectHtmlArr.join('')}
+    ${projectsArr
+      .filter(({ feature }) => feature)
+      .map(({ name, description, languages, link }) => {
+        return `
+        <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+          <h3 class="portfolio-item-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(", ")}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+        `;
+      })
+      .join("")}
+
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(", ")}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+          `;
+        })
+        .join("")}
     </div>
   </section>
   `;
