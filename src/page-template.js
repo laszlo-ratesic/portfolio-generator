9fd4d3da2a3320b6e49820d9e795ca7c8b97af4b
laszlo-ratesic <keenanrc@hotmail.com>
2022-02-16 08:41:48 -0600

    add filter

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,76 @@
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
+
+  return `
+  <section class="my-3" id="about">
+    <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+    <p>${aboutText}</p>
+  </section>
+  `;
+};
+
+const generateProjects = projectsArr => {
+  // get array of just featured projects
+  const featuredProjects = projectsArr.filter(project => {
+    if (project.feature) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  // get array of all non-featured projects
+  const nonFeatuedProjects = projectsArr.filter(project => {
+    if (!project.feature) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+    return `
+    <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+      <h3 class="portfolio-item-title text-light">${name}</h3>
+      <h5 class="portfolio-languages">
+        Built With:
+        ${languages.join(', ')}
+      </h5>
+      <p>${description}</p>
+      <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+    </div>
+    `;
+  });
+
+  const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
+    ({ name, description, languages, link }) => {
+      return `
+      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+        <h3 class="portfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-languages">
+          Built With:
+          ${languages.join(', ')}
+        </h5>
+        <p>${description}</p>
+        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+      </div>
+      `;
+    }
+  );
+
+  return `
+  <section class="my-3" id="portfolio">
+    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+    <div class="flex-row justify-space-between">
+    ${featuredProjectHtmlArr.join('')}
+    ${nonFeaturedProjectHtmlArr.join('')}
+    </div>
+  </section>
+  `;
+};
+
 module.exports = (templateData) => {
   // Destructure page data by section
   const { projects, about, ...header } = templateData;
@@ -35,7 +108,8 @@ module.exports = (templateData) => {
         </div>
       </header>
       <main class="container my-5">
-
+              ${generateAbout(about)}
+              ${generateProjects(projects)}
       </main>
       <footer class="container text-center py-3">
           <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
